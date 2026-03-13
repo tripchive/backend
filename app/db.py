df21592c0520666aecb5ed46fb637efefcd2263b
Tom-the-Bomb <thello522@gmail.com>
2026-03-13 02:24:44 -0400

    time to RWIIR

--- app/db.py
+++
@@ -1,9 +0,0 @@
-import asyncpg
-
-
-async def create_pool(url: str) -> asyncpg.Pool:
-    return await asyncpg.create_pool(dsn=url)
-
-
-async def close_pool(pool: asyncpg.Pool) -> None:
-    await pool.close()
